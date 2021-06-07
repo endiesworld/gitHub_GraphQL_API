ea8ec20f1fca2aa537936f02aced2405027c9c9f
endiesworld <endiesworld@gmail.com>
2021-06-07 11:35:51 +0100

    feat: Updated and added tailwindcss to the dsiplay page

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
-const express = require("express")
-const app = express()
+const express = require("express"); 
+const fetch = require("node-fetch") ;
 
-app.use(express.static("public"))
+const app = express() ;
 
-app.get("/data", (req, res) => {
-    res.end("Response from the API!")
+app.use(express.static("public"));
+
+const url = "https://api.github.com/graphql" ;
+const query = ` {
+    search(query: "stars:>50000", type: REPOSITORY, first: 10) {
+        repositoryCount
+        edges {
+            node {
+            ... on Repository {
+                name
+                owner {
+                login
+                }
+                stargazers {
+                totalCount
+                }
+            }
+            }
+        }
+        }
+  }
+  ` ;
+
+const options = {
+    method: "post",
+    headers: {
+        "content-type": "application/json",
+        authorization: "bearer " + process.env.APIKEY,
+    },
+    body: JSON.stringify({ query: query }),
+}
+
+app.get("/data", async (req, res) => {
+    let response
+    try {
+        response = await fetch(url, options);
+    } catch (error) {
+        console.error(error)
+    }
+    const data = await response.json() ;
+    res.json(data);
 })
 
 app.listen(3000, () => console.log("Server ready"))
