5347ef1698a6e7150a2d2c34c055a5e17de8f158
endiesworld <endiesworld@gmail.com>
2021-06-07 08:01:48 +0100

    feat: Added node-fetch library

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 
 app.use(express.static("public"))
 
-
+app.get("/data", (req, res) => {
+    res.end("Response from the API!")
+})
 
 app.listen(3000, () => console.log("Server ready"))
