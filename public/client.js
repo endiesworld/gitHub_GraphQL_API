3c32216ac345965cd04850d99b46232ebd9572ab
endiesworld <endiesworld@gmail.com>
2021-07-17 16:50:40 +0100

    feat:Added mobile option

--- public/client.js
+++ public/client.js
@@ -5,14 +5,14 @@ document.addEventListener("DOMContentLoaded", async () => {
     document.querySelector("tbody").insertAdjacentHTML(
         "beforeend",
         `
-            <tr>
-            <td class="px-6 border-b py-4 whitespace-no-wrap font-black border-indigo-800 text-sm leading-5 font-medium text-white">
+            <tr class="h-24 lg:h-16">
+            <td class="px-6 border-b py-4 whitespace-no-wrap border-indigo-800 text-3xl lg:text-xs font-bold lg:text-white ">
                 ${item.node.owner.login}
             </td>
-            <td class="px-6 border-b py-4 whitespace-no-wrap border-indigo-800 text-sm leading-5 text-white">
+            <td class="px-6 border-b py-4 whitespace-no-wrap border-indigo-800 text-3xl lg:text-xs font-bold lg:text-white ">
                 ${item.node.name}
             </td>
-            <td class="px-6 border-b py-4 whitespace-no-wrap border-indigo-800 text-sm leading-5 text-white">
+            <td class="px-6 border-b py-4 whitespace-no-wrap border-indigo-800 text-3xl lg:text-xs font-bold lg:text-white ">
                 ${item.node.stargazers.totalCount}
             </td>
             </tr>
